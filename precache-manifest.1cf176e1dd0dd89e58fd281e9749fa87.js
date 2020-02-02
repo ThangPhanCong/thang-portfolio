5ba4ff6c2d0cac6088a48d419925ecd4884ef76e
ThangPhan <thangphan@ThangPhannoMacBook-Pro.local>
2020-02-02 12:03:05 +0900

    Updates

---
+++ precache-manifest.1cf176e1dd0dd89e58fd281e9749fa87.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b7261a8bdaa4c559adf23e4e9b7d66e",
+    "url": "/thang-portfolio/index.html"
+  },
+  {
+    "revision": "a60334a812eb82309004",
+    "url": "/thang-portfolio/static/css/main.30bc78c9.chunk.css"
+  },
+  {
+    "revision": "87a01c369fc1988dc272",
+    "url": "/thang-portfolio/static/js/2.fdff4a80.chunk.js"
+  },
+  {
+    "revision": "a60334a812eb82309004",
+    "url": "/thang-portfolio/static/js/main.62407b47.chunk.js"
+  },
+  {
+    "revision": "3f03062c53ef093a46f7",
+    "url": "/thang-portfolio/static/js/runtime-main.511e0666.js"
+  },
+  {
+    "revision": "ee2c2964926a62011d1b2389884d401c",
+    "url": "/thang-portfolio/static/media/background.ee2c2964.jpg"
+  },
+  {
+    "revision": "acc71218dd788828abeda273de9eea19",
+    "url": "/thang-portfolio/static/media/kyoto.acc71218.jpg"
+  },
+  {
+    "revision": "2a3005d0571251833fba4e5c93d5b346",
+    "url": "/thang-portfolio/static/media/musics.2a3005d0.jpg"
+  },
+  {
+    "revision": "54cc6ea80764339737312cac6a583cdc",
+    "url": "/thang-portfolio/static/media/nagano.54cc6ea8.jpg"
+  },
+  {
+    "revision": "4bb3ef8106430dd16f7f9729bfe1c178",
+    "url": "/thang-portfolio/static/media/osaka.4bb3ef81.jpg"
+  },
+  {
+    "revision": "1746d1d1ca5a8e0ed4e297bbac0145bb",
+    "url": "/thang-portfolio/static/media/profile.1746d1d1.jpg"
+  },
+  {
+    "revision": "fc5b3a2090cee9a706dcf6a7a0e07a5d",
+    "url": "/thang-portfolio/static/media/tokyo.fc5b3a20.jpg"
+  },
+  {
+    "revision": "283319cd8844b0a688f19fd17ffc2f89",
+    "url": "/thang-portfolio/static/media/trips.283319cd.jpg"
+  }
+]);
